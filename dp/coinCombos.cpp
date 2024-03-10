264217af3d3244840dbdb2dc01e98d4df54365d8
Charles Rankine <charlie.rankine@gmail.com>
2024-03-10 23:50:00 +1030

    Fix a bunch of files

--- dp/coinCombos.cpp
+++ dp/coinCombos.cpp
@@ -4,26 +4,34 @@ using namespace std;
 
 class Solution {
 public:
-    int dpTest(int goal, vector<int> coins) {
-        int dp[goal+1] = { 0 };
-        dp[0] = 1;
+  int dpTest(int goal, vector<int> &coins) {
+    vector<int> dp(goal + 1, 0);
+    dp[0] = 1;
 
-        for (int i = 0; i <= goal; ++i) {
-            for (int coin : coins) {
-                if ((i - coin) < 0)
-                    continue;
+    for (int i = 0; i <= goal; ++i) {
+      for (int coin : coins) {
+        if ((i - coin) < 0)
+          continue;
 
-                dp[i] += dp[i-coin];
-            }
-            cout << "i: " << i << " | dp[i]: " << dp[i] << endl;
-        }
-        
-        return dp[goal];
+        dp[i] += dp[i - coin];
+      }
+      cout << "i: " << i << " | dp[i]: " << dp[i] << endl;
     }
+
+    return dp[goal];
+  }
 };
 
 int main() {
-    Solution s;
-    cout << s.dpTest(20, { 1, 4, 5 }) << endl;
-}
+  Solution s;
+  vector<int> coins;
+  int goal;
 
+  coins = {1, 4, 5};
+  goal = 20;
+  cout << "goal: " << goal << " coins: ";
+  for (int &coin : coins)
+    cout << coin << " ";
+  cout << endl;
+  cout << s.dpTest(goal, coins) << endl;
+}
--- dp/rabbitGrid.cpp
+++ dp/rabbitGrid.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 class Solution {
 public:
-    int dpTest(int columns, int rows) {
-        int dp[columns][rows];
+  int dpTest(int columns, int rows) {
+    int dp[columns][rows];
 
-        for (int y = 0; y < rows; ++y)
-            for (int x = 0; x < columns; ++x) {
-                if (x - 1 < 0 || y - 1 < 0)
-                    dp[x][y] = 1;
-                else
-                    dp[x][y] = dp[x-1][y] + dp[x][y-1];
-            }
-        
-        for (int y = 0; y < rows; ++y){
-            for (int x = 0; x < columns; ++x)
-                cout << dp[x][y] << " ";
-            cout << endl;
-        }
+    for (int y = 0; y < rows; ++y)
+      for (int x = 0; x < columns; ++x) {
+        if (x - 1 < 0 || y - 1 < 0)
+          dp[x][y] = 1;
+        else
+          dp[x][y] = dp[x - 1][y] + dp[x][y - 1];
+      }
 
-        return dp[columns-1][rows-1];
+    for (int y = 0; y < rows; ++y) {
+      for (int x = 0; x < columns; ++x)
+        cout << dp[x][y] << " ";
+      cout << endl;
     }
+
+    return dp[columns - 1][rows - 1];
+  }
 };
 
 int main() {
-    int cin1, cin2;
-	cout << "Enter x, y seperated by a space: ";
-    cin >> cin1 >> cin2;
-	
-	Solution s;
-	int result = s.dpTest(cin1, cin2);
-    cout << "Result: " << result << endl;
-}
+  Solution s;
 
+  int cin1, cin2;
+  cout << "Enter x, y seperated by a space: ";
+  cin >> cin1 >> cin2;
+
+  int result = s.dpTest(cin1, cin2);
+  cout << "Result: " << result << endl;
+}
